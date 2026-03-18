27042cbe585a7c783d1114604c030437c9343870
Mohammed Hindi <moh5408415@gmail.com>
2026-03-18 09:29:45 +0200

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('books', BookController::class);
+Route::get("dashboard", function () {
+    return view("layouts.app");
+})->name("dashboard");
+
+Route::get("login", [AuthController::class, "login"])->name("login");
+Route::post("login", [AuthController::class, "authenticate"])->name("authenticate");
+Route::post("logout", [AuthController::class, "logout"])->name("logout");
